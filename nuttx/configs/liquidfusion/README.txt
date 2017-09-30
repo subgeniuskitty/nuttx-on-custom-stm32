802ded2c7537f4c6c4e4e104527dabb8dbe6d3ec
Aaron Taylor <ataylor@subgeniuskitty.com>
2017-09-30 09:01:14 +0000

    Broke dependencies on config/shenzhou. Everything is now under liquidfusion branch.

--- nuttx/configs/liquidfusion/README.txt
+++ nuttx/configs/liquidfusion/README.txt
@@ -6,6 +6,5 @@ This README covers Liquid Fusion (SGK-PCB-0013 REV-002).
 TODO
 ====
 
-Remove SHENZHOU references in #DEFINES
 Use LEDs for startup feedback
 Configure nsh to listen for TELNET connections
--- nuttx/configs/liquidfusion/include/board.h
+++ nuttx/configs/liquidfusion/include/board.h
@@ -1,40 +1,5 @@
-/************************************************************************************
- * configs/shenzhou/include/board.h
- *
- *   Copyright (C) 2012 Gregory Nutt. All rights reserved.
- *   Author: Gregory Nutt <gnutt@nuttx.org>
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- * 3. Neither the name NuttX nor the names of its contributors may be
- *    used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- ************************************************************************************/
-
-#ifndef __CONFIGS_SHENZHOU_INCLUDE_BOARD_H
-#define __CONFIGS_SHENZHOU_INCLUDE_BOARD_H 1
+#ifndef __CONFIGS_LIQUIDFUSION_INCLUDE_BOARD_H
+#define __CONFIGS_LIQUIDFUSION_INCLUDE_BOARD_H 1
 
 /************************************************************************************
  * Included Files
@@ -166,21 +131,10 @@ extern "C" {
 
 void stm32_boardinitialize(void);
 
-/************************************************************************************
- * Chip ID functions
- *
- * Description:
- *   Non-standard functions to obtain chip ID information.
- *
- ************************************************************************************/
-
-const char *stm32_getchipid(void);
-const char *stm32_getchipid_string(void);
-
 #undef EXTERN
 #if defined(__cplusplus)
 }
 #endif
 
 #endif /* __ASSEMBLY__ */
-#endif /* __CONFIGS_SHENZHOU_INCLUDE_BOARD_H */
+#endif /* __CONFIGS_LIQUIDFUSION_INCLUDE_BOARD_H */
